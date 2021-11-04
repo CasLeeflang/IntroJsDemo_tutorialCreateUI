97afbcc7d2d3fdc3f0319b675bab0f431cd4a913
Imani <455181@student.fontys.nl>
2021-11-04 11:20:37 +0100

    Styling createstep file

--- src/Modules/Tutorial/Components/CreateStep.vue
+++ src/Modules/Tutorial/Components/CreateStep.vue
@@ -1,86 +1,137 @@
 <template>
   <div>
     <h1>Create a new tutorial here</h1>
-
     <div>
+
       <form id="myForm" v-on:submit.prevent="add">
+        <div class="d-md-inline-flex flex-column" style="width: 40%">
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">Title</span>
+            <input type="text" id="title" class="form-control" aria-label="Username"
+                   aria-describedby="basic-addon1" v-model="form.title">
+          </div>
 
-        <div class="form-group">
-          <label>Title</label>
-          <input type="text" id="title" v-model="form.title">
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon2">Element</span>
+            <input type="text" id="element" class="form-control" aria-label="Username"
+                   aria-describedby="basic-addon1" v-model="form.element">
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon3">Intro</span>
+            <input type="text" id="intro" class="form-control lg" aria-label="Username"
+                   aria-describedby="basic-addon1" v-model="form.intro">
+          </div>
+
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon4">Permission</span>
+            <input type="text" id="permission" class="form-control" aria-label="Username"
+                   aria-describedby="basic-addon1" v-model="form.permission">
+          </div>
         </div>
 
-        <div class="form-group">
-          <label>Element</label>
-          <input type="text" id="element" v-model="form.element">
-        </div>
 
         <div class="form-group">
-          <label>Intro</label>
-          <input type="text" id="intro" v-model="form.intro">
-        </div>
-
-        <div class="form-group">
-          <label>Permission</label>
-          <input type="text" id="permission" v-model="form.permission">
-        </div>
-        <div class="form-group">
-          <button class="btn btn-primary">Add</button>
+          <button class="btn btn-primary btn-lg p-1" style="width: 10%">Add</button>
         </div>
 
       </form>
     </div>
 
+    <div class="p-5">
+      <h2 class="p-1">New tutorial</h2>
+      <button class="btn btn-primary btn-lg p-1" style="width: 10%" v-on:click="save">Save tutorial</button>
+      <tr v-for="(step, index) in newSteps" :key="index">
 
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-5">
+                Title
+              </div>
+              <div class="col-5">
+                {{ step.title }}
+              </div>
+              <div class="col-5">
+                Element
+              </div>
+              <div class="col-5">
+                {{ step.element }}
+              </div>
+              <div class="col-5">
+                Intro
+              </div>
+              <div class="col-5">
+                {{ step.intro }}
+              </div>
+              <div class="col-5">
+                Permission
+              </div>
+              <div class="col-5">
+                {{ step.permission }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </tr>
 
+      <h2 class="p-2">Existing tutorials</h2>
+      <div class="row">
+        <div class="col-sm-3">
+          <tr v-for="(tutorial, index) in existingTutorials" :key="index">
 
+            <div class="card" style="width: 18rem;">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-5">
+                    Title:
+                  </div>
+                  <div class="col-5">
+                    {{ tutorial.title }}
+                  </div>
+                  <div class="col-5">
+                    Number of steps:
+                  </div>
+                  <div class="col-5">
+                    {{ tutorial.steps.length }}
+                  </div>
+                  <div class="col-5">
+                    Category:
+                  </div>
+                  <div class="col-5">
+                    {{ tutorial.category }}
+                  </div>
+                </div>
+              </div>
+            </div>
 
-
-
-<h2>new tutorial</h2>
-    <button v-on:click="save">Save tutorial</button>
-  <tr v-for="(step, index) in newSteps" :key="index">
-    <div>
-    Title: <td>{{step.title}}</td>
-    Element: <td>{{step.element}}</td>
-    Step: <td>{{step.intro}}</td>
-    Permission: <td>{{step.permission}}</td>
-    </div>
-    <p>--------------------------------------------------------------</p>
-  </tr>
-
-    <h2>Existing tutorials</h2>
-    <tr v-for="(tutorial, index) in existingTutorials" :key="index">
-      <div>
-        Title: <td>{{tutorial.title}}</td>
-        Number of steps: <td>{{tutorial.steps.length}}</td>
-        category: <td>{{tutorial.category}}</td>
+          </tr>
+        </div>
       </div>
-      <p>--------------------------------------------------------------</p>
-    </tr>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+
 export default {
   name: "CreateTutorial",
   data() {
-      return{
-        form: {},
-        newSteps:[],
-        tutorial: {
-          title: null,
-          steps: null,
-          category: null
-        },
-        existingTutorials:[],
+    return {
+      form: {},
+      newSteps: [],
+      tutorial: {
+        title: null,
+        steps: null,
+        category: null
+      },
+      existingTutorials: [],
 
     }
   },
   created() {
     axios.get("http://localhost:9090/api/tutorial/getTut")
-    .then(response => this.existingTutorials = response.data);
+        .then(response => this.existingTutorials = response.data);
   },
 
   methods: {
@@ -94,7 +145,7 @@ export default {
       this.newSteps.push(newStep);
     },
 
-    save: function(){
+    save: function () {
       this.tutorial.steps = this.newSteps;
       axios.post("http://localhost:9090/api/tutorial/new", this.tutorial)
       location.reload();
