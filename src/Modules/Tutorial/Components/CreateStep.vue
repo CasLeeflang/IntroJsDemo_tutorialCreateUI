971bae46b7142c363c330cc077a237cac5f1d3dd
Cas Leeflang <466933@student.fontys.nl>
2021-11-04 09:54:34 +0100

    Added dashboard for things

---
+++ src/Modules/Tutorial/Components/CreateStep.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <h1>Create a new tutorial here</h1>
+
+    <div>
+      <form id="myForm" v-on:submit.prevent="add">
+
+        <div class="form-group">
+          <label>Title</label>
+          <input type="text" id="title" v-model="form.title">
+        </div>
+
+        <div class="form-group">
+          <label>Element</label>
+          <input type="text" id="element" v-model="form.element">
+        </div>
+
+        <div class="form-group">
+          <label>Intro</label>
+          <input type="text" id="intro" v-model="form.intro">
+        </div>
+
+        <div class="form-group">
+          <label>Permission</label>
+          <input type="text" id="permission" v-model="form.permission">
+        </div>
+        <div class="form-group">
+          <button class="btn btn-primary">Add</button>
+        </div>
+
+      </form>
+    </div>
+
+
+
+
+
+
+
+<h2>new tutorial</h2>
+    <button v-on:click="save">Save tutorial</button>
+  <tr v-for="(step, index) in newSteps" :key="index">
+    <div>
+    Title: <td>{{step.title}}</td>
+    Element: <td>{{step.element}}</td>
+    Step: <td>{{step.intro}}</td>
+    Permission: <td>{{step.permission}}</td>
+    </div>
+    <p>--------------------------------------------------------------</p>
+  </tr>
+
+    <h2>Existing tutorials</h2>
+    <tr v-for="(tutorial, index) in existingTutorials" :key="index">
+      <div>
+        Title: <td>{{tutorial.title}}</td>
+        Number of steps: <td>{{tutorial.steps.length}}</td>
+        category: <td>{{tutorial.category}}</td>
+      </div>
+      <p>--------------------------------------------------------------</p>
+    </tr>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: "CreateTutorial",
+  data() {
+      return{
+        form: {},
+        newSteps:[],
+        tutorial: {
+          title: null,
+          steps: null,
+          category: null
+        },
+        existingTutorials:[],
+
+    }
+  },
+  created() {
+    axios.get("http://localhost:9090/api/tutorial/getTut")
+    .then(response => this.existingTutorials = response.data);
+  },
+
+  methods: {
+    add: function () {
+      let newStep = {
+        title: this.form.title,
+        element: this.form.element,
+        intro: this.form.intro,
+        permission: this.form.permission
+      }
+      this.newSteps.push(newStep);
+    },
+
+    save: function(){
+      this.tutorial.steps = this.newSteps;
+      axios.post("http://localhost:9090/api/tutorial/new", this.tutorial)
+      location.reload();
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/Modules/Tutorial/Components/CreateTutorial.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Create a new Tutorial</h1>
+
+    <div>
+      <form id="myForm" v-on:submit.prevent="addSteps">
+
+        <div class="form-group">
+          <label>Title</label>
+          <input type="text" id="title" v-model="form.title">
+        </div>
+
+        <div class="form-group">
+          <label>Category</label>
+          <input type="text" id="permission" v-model="form.category">
+        </div>
+
+        <div class="form-group">
+          <button class="btn btn-primary">Add</button>
+        </div>
+
+      </form>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "CreateTutorial",
+  methods: function () {
+    this.$router.push({
+      path: '/pagename',
+      params: {
+        param1: 'value1',
+        param2: 'value2'
+      }
+    });
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
